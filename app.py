77fbf1dba23397ab349aaa3564178201f16074a6
Daniel <filind85@gmail.com>
2019-07-17 19:14:52 +0300

    some work is on

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 from flask import Flask, request, send_from_directory
 import weather
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-
-
-
-
+CORS(app)
 
 @app.route('/weather/<city>')
 def get_weather(city):
     data = weather.get_weather(city)
-    return str((data['main']['temp'], data['main']['humidity'], data['weather'][0]['description']))
-
+    x = str((data['main']['temp'], data['main']['humidity'], data['weather'][0]['description']))
+    return x
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='localhost', port=8080)
--- static/index.js
+++ static/index.js
@@ -2,10 +2,19 @@ hackaton = {}
 
 hackaton.getWeather = function() {
     var cityName = $("#cityWeather").val();
+    console.log(cityName)
+
+    // $.get("localhost:8080/weather/" + cityName,
+    //  function(data, status){
+    //     alert("Data: " + data + "\nStatus: " + status);
+    //   });
+
         $.ajax({
             type: "GET",
-            dataType: 'json',
-            url: ("localhost/weather/" + cityName),
+            crossDomain: true,
+            dataType: "json",
+            url: ("localhost:8080/weather/" + cityName),
+            
             success: function (response) {
                 console.log(response)
             },
