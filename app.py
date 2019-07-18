23d2307cf2405194d0894cfa93530b7683827ada
Daniel <filind85@gmail.com>
2019-07-18 11:15:32 +0300

    work on html, css, ajax

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, request, send_from_directory
 import weather
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app, resources={r"localhost:8080/weather/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/weather/<city>')
+
+
+@app.route('/weather/<city>', methods=['GET','POST'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def get_weather(city):
     data = weather.get_weather(city)
+    data.headers.add('Access-Control-Allow-Origin', '*')
     x = str((data['main']['temp'], data['main']['humidity'], data['weather'][0]['description']))
     return x
 
